68d63a91ad4037390a7d435e1a3fe82bdb39369d
jeasue <163128877+jeasue@users.noreply.github.com>
2024-05-25 07:14:31 +0000

    20240525

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('hello');
+//주석처리
+alert('방가방가');
+document.write('다큐몬트에요');
+var a = 10;
+var b;
+b=100;
+console.log(a+b);
+console.log(7%4);
+console.log(7==7);
+console.log(7!=7);
+console.log(7==='7');
+console.log(7+parseInt('2',10));
+console.log((3).toString());
+var name = '히히히';
+console.log(`안녕하세요, ${name}`);
+var p = 'cat dog hamster alligator';
+console.log(p.indexOf('dog'));
